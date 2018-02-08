73761af192f0f44430ec2e9700d381922cd6f41f
hungdangtung <hung.dangtung@yahoo.com.vn>
2018-02-08 22:25:27 +0700

    liquibase work but could not start

--- src/main/java/com/country/city/CountryCityApplication.java
+++ src/main/java/com/country/city/CountryCityApplication.java
@@ -1,9 +1,13 @@
 package com.country.city;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,WebMvcAutoConfiguration.class })
 public class CountryCityApplication {
 
 	public static void main(String[] args) {
